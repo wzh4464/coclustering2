ec7208f0423087c49de02ea71fa14271fa2e43f7
Hance Wu <32484940+wzh4464@users.noreply.github.com>
2023-07-05 12:49:21 +0800

    mycluster1 changed

--- main.py
+++ main.py
@@ -3,46 +3,75 @@ import h5py
 import scipy.io
 from scipy.linalg import svd
 
-def mycluster1(Data, NumKind, maxstep, load_svd=False, svd_file=None):
+
+def mycluster1(Data, NumKind, maxstep):
     DataRow, DataColumn = Data.shape
     Center = np.zeros((DataRow, NumKind))
     B = np.zeros((NumKind, DataColumn))
     COV = np.zeros((DataRow, DataRow, NumKind))
-    Y = np.zeros(DataColumn)
-    Ynew = np.zeros_like(Y)
+    Y = np.zeros(DataColumn, dtype=int)
+    Ynew = Y.copy()
 
+    # N = DataColumn // NumKind calculates the number of data points to assign to each cluster.
     N = DataColumn // NumKind
-    if load_svd and svd_file is not None:
-        with h5py.File(svd_file, 'r') as file:
-            U = np.array(file['U'])
-            S = np.array(file['S'])
-            V = np.array(file['V'])
-    else:
-        U, S, V = np.linalg.svd(Data)
-    enga = U[:, 0].dot(Data)
 
-    Yenga, I = np.sort(enga), np.argsort(enga)
+    U, _, _ = np.linalg.svd(Data)
+
+    # Computes the projection of the input data matrix onto the first principal component.
+    enga = U[:, 0].T @ Data
+
+    # Sorts the projection of the input data matrix onto the first principal component in ascending order and returns the sorted indices.
+    I = np.argsort(enga)
+
+    # This loop assigns the remaining data points to the clusters based on the sorted indices of the projection of the input data matrix onto the first principal component.
+    # It iterates over the number of clusters and assigns the next N data points to each cluster.
+    # 这个循环根据将输入数据矩阵投影到第一个主成分的排序索引，将剩余的数据点分配给聚类。
+    # 它遍历聚类数量，并将接下来的N个数据点分配给每个聚类。
     for i in range(NumKind):
-        Ynew[I[N*i:N*(i+1)]] = i
-    Ynew[I[N*(NumKind-1):]] = NumKind - 1
+        Ynew[I[N * i : N * (i + 1)]] = i + 1
+
+    # Assign the last few data points to the last cluster.
+    # Avoiding the case where the number of data points is not divisible by the number of clusters.
+    Ynew[I[N * (NumKind - 1) :]] = NumKind
 
     for step in range(maxstep):
         if np.sum(Ynew != Y) == 0:
+            # If the cluster assignments do not change, then
             break
+            # stop the iteration.
         else:
+            # Otherwise, update the cluster assignments.
             Y = Ynew.copy()
+            # Update the cluster centers.
             for i in range(NumKind):
-                Center[:, i] = np.mean(Data[:, Y == i], axis=1)
+                NewCenter = np.mean(Data[:, Y == i + 1], axis=1)
+                # Update the cluster centers with the mean of the data points assigned to each cluster.
+                Center[:, i] = NewCenter
+
             for i in range(NumKind):
-                COV[:, :, i] = np.cov(Data[:, Y == i])
+                COV[:, :, i] = np.cov(Data[:, Y == i + 1], rowvar=True)
+                # Update the covariance matrix of each cluster.
+
             B = np.zeros((NumKind, DataColumn))
+            # B is the probability of each data point belonging to each cluster.
+
             for i in range(NumKind):
                 for j in range(DataColumn):
-                    dist = Data[:, j] - Center[:, i]
-                    B[i, j] = multivariate_normal.pdf(Data[:, j], mean=Center[:, i], cov=COV[:, :, i])
-            Ynew = np.argmax(B, axis=0)
+                    d_ij = Data[:, j] - Center[:, i]
+                    B[i, j] = (
+                        1
+                        / (2 * np.pi * np.sqrt(np.linalg.det(COV[:, :, i])))
+                        * np.exp(-0.5 * d_ij.T @ np.linalg.inv(COV[:, :, i]) @ d_ij)
+                    )
+                    # Update the probability of each data point belonging to each cluster.
+                    # Note Data[:,j]-Center[:,i] as d_ij.
+                    # B(i,j)=\frac{1}{\sqrt{2\pi}\sqrt{det(Cov_i)}}exp(-\frac{1}{2}d_{ij}^T*Cov_i^{-1}*d_{ij})
 
-    return Center, Y, step
+            # Assign each data point to the cluster with the highest probability.
+            Ynew = np.argmax(B, axis=0) + 1
+
+    return Center, Y, step + 1
+
 
 def findindex(I, Ngroup):
     Nout = []
@@ -52,21 +81,22 @@ def findindex(I, Ngroup):
     Nc = np.zeros(N, dtype=int)
     Ic = []
     Jc = []
-    a = np.arange(2, Ngroup+1)
+    a = np.arange(2, Ngroup + 1)
     aa = a**2
-    Nc[:aa[0]] = a[0]
+    Nc[: aa[0]] = a[0]
     for i in range(1, len(a)):
         Ntemp = np.sum(aa[:i])
-        Nc[Ntemp:Ntemp+aa[i]] = a[i]
-    for i in range(2, len(a)+2):
-        for j in range(1, i+1):
-            Ic.extend([j]*i)
-            Jc.extend(list(range(1, i+1)))
+        Nc[Ntemp : Ntemp + aa[i]] = a[i]
+    for i in range(2, len(a) + 2):
+        for j in range(1, i + 1):
+            Ic.extend([j] * i)
+            Jc.extend(list(range(1, i + 1)))
     for i in range(N):
-        Nout.append(Nc[I[i]-1])
-        Iout.append(Ic[I[i]-1])
-        Jout.append(Jc[I[i]-1])
+        Nout.append(Nc[I[i] - 1])
+        Iout.append(Ic[I[i] - 1])
+        Jout.append(Jc[I[i] - 1])
     return np.array(Nout), np.array(Iout), np.array(Jout)
+
 
 def svdbicluster(data, dim, Num_cluster, read_uv=False):
     scaledata = data
@@ -81,9 +111,9 @@ def svdbicluster(data, dim, Num_cluster, read_uv=False):
     for d in dim:
         if read_uv:
             # read from 'UV.mat'
-            with h5py.File('UV.mat', 'r') as f:
-                u = f['u'][:]
-                v = f['v'][:]
+            with h5py.File("UV.mat", "r") as f:
+                u = f["u"][:]
+                v = f["v"][:]
         else:
             u = U[:, :d]
             v = V[:, :d]
@@ -93,8 +123,8 @@ def svdbicluster(data, dim, Num_cluster, read_uv=False):
         indexu = np.zeros((Ngroup, mf), dtype=int)
         indexv = np.zeros((Ngroup, nf), dtype=int)
         for n in range(2, Ngroup):
-            pointeru, _ = mycluster1(u.T, n, 100)
-            pointerv, _ = mycluster1(v.T, n, 100)
+            pointeru, _, _ = mycluster1(u.T, n, 100)
+            pointerv, _, _ = mycluster1(v.T, n, 100)
             indexu[n, :] = pointeru
             indexv[n, :] = pointerv
             for i in range(n):
@@ -114,16 +144,17 @@ def svdbicluster(data, dim, Num_cluster, read_uv=False):
     columcluster = vicell
     return rowcluster, columcluster
 
+
 def main():
-    mat = scipy.io.loadmat('A.mat')
+    mat = scipy.io.loadmat("A.mat")
     dim = 5
     Num_co_cluster = 8
-    data = mat['A']
+    data = mat["A"]
     rowcluster, columcluster = svdbicluster(data, [dim], Num_co_cluster, read_uv=True)
 
     print(rowcluster)
     print(columcluster)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- mycluster1.m
+++ mycluster1.m
@@ -1,44 +1,83 @@
-function [Center, Y, step]=mycluster1(Data,NumKind,maxstep)
+% FILEPATH: /Users/zihanwu/codes/coclustering2/mycluster1.m
+% MYCLUSTER1 performs co-clustering on the input data using the specified number of clusters.
+%
+% INPUTS:
+% Data - input data matrix
+% NumKind - number of clusters to form
+% maxstep - maximum number of iterations to perform
+%
+% OUTPUTS:
+% Center - final cluster centers
+% Y - final cluster assignments for each data point
+% step - number of iterations performed
 
-[DataRow,DataColumn]=size(Data);
-Center = zeros(DataRow,NumKind);%
-B=zeros(NumKind,DataColumn); %
-COV=zeros(DataRow,DataRow,NumKind);%
-Y=zeros(1,DataColumn);%
-Ynew=Y;%
+function [Center, Y, step] = mycluster1(Data, NumKind, maxstep)
 
-N = floor(DataColumn/NumKind);
-[U S V] = svd(Data);
-enga = U(:,1)'*Data;
+    [DataRow, DataColumn] = size(Data);
+    Center = zeros(DataRow, NumKind);
+    B = zeros(NumKind, DataColumn);
+    COV = zeros(DataRow, DataRow, NumKind);
+    Y = zeros(1, DataColumn);
+    Ynew = Y;
 
-[Yenga,I] = sort(enga);
-for i=1:NumKind
-    Ynew(I([1:N]+N*(i-1)))=i;
-end
-Ynew(I((1+N*(NumKind-1)):DataColumn))=NumKind;
+    % N = floor(DataColumn/NumKind) calculates the number of data points to assign to each cluster.
+    N = floor(DataColumn / NumKind);
 
-for step = 1:maxstep
-    if sum(Ynew~=Y)==0 break
-    else
-        Y = Ynew;
-        for i=1:NumKind
-            NewCenter(:,i)=mean(Data(:,Y==i),2);
-        end
-        Center=NewCenter;
-        for i=1:NumKind
-                COV(:,:,i)=cov(Data(:,Y==i)');
-        end
-        B=zeros(NumKind,DataColumn); 
-        for i = 1:NumKind
-            for j = 1:DataColumn
-                B(i,j)=1/(2*pi*(det(COV(:,:,i)))^0.5)*exp(-0.5*(Data(:,j) ...,
-                    -Center(:,i))'*COV(:,:,i)^-1*(Data(:,j)-Center(:,i)));
-            end
-        end
+    [U, ~, ~] = svd(Data);
 
-        [X,Ynew]= max(B);  
+    % Computes the projection of the input data matrix onto the first principal component.
+    enga = U(:, 1)' * Data;
 
+    % Sorts the projection of the input data matrix onto the first principal component in ascending order and returns the sorted indices.
+    [~, I] = sort(enga);
+
+    % This loop assigns the remaining data points to the clusters based on the sorted indices of the projection of the input data matrix onto the first principal component.
+    % It iterates over the number of clusters and assigns the next N data points to each cluster.
+    % 这个循环根据将输入数据矩阵投影到第一个主成分的排序索引，将剩余的数据点分配给聚类。
+    % 它遍历聚类数量，并将接下来的N个数据点分配给每个聚类。
+    for i = 1:NumKind
+        Ynew(I([1:N] + N * (i - 1))) = i;
     end
-end
-% 
 
+    % Assign the last few data points to the last cluster.
+    % Avoiding the case where the number of data points is not divisible by the number of clusters.
+    Ynew(I((1 + N * (NumKind - 1)):DataColumn)) = NumKind;
+
+    for step = 1:maxstep
+
+        if sum(Ynew ~= Y) == 0
+            % If the cluster assignments do not change, then
+            break
+            % stop the iteration.
+        else
+            % Otherwise, update the cluster assignments.
+            Y = Ynew;
+            % Update the cluster centers.
+            for i = 1:NumKind
+                NewCenter(:, i) = mean(Data(:, Y == i), 2);
+                % Update the cluster centers with the mean of the data points assigned to each cluster.
+            end
+
+            Center = NewCenter;
+
+            for i = 1:NumKind
+                COV(:, :, i) = cov(Data(:, Y == i)');
+                % Update the covariance matrix of each cluster.
+            end
+
+            B = zeros(NumKind, DataColumn);
+            % B is the probability of each data point belonging to each cluster.
+
+            for i = 1:NumKind
+                for j = 1:DataColumn
+                    B(i, j) = 1 / (2 * pi * (det(COV(:, :, i))) ^ 0.5) * exp(-0.5 * (Data(:, j) ... ,
+                        -Center(:, i))' * COV(:, :, i) ^ -1 * (Data(:, j) - Center(:, i)));
+                    % Update the probability of each data point belonging to each cluster.
+                    % Note Data(:,j)-Center(:,i) as d_ij.
+                    % B(i,j)=\frac{1}{\sqrt{2\pi}\sqrt{det(Cov_i)}}exp(-\frac{1}{2}d_{ij}^T*Cov_i^{-1}*d_{ij})
+                end
+            end
+            % Assign each data point to the cluster with the highest probability.
+            [X, Ynew] = max(B);
+        end
+    end
